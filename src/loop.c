52684c0dec1c725df7c3e442ceafbac58db2eb31
alexionut-tincu <alexionut05@users.noreply.github.com>
2025-01-14 16:29:56 +0200

    Added cwd display and colored display

--- src/loop.c
+++ src/loop.c
@@ -10,10 +10,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include "builtins.h"
 #include "execute.h"
 #include "loop.h"
 #include "parse.h"
+
+#define CWD_BUF_SZ 1024
+
+/**
+ * @brief The color codes for the terminal
+ * @param color The color to get the code for
+ * @return The color code
+ */
+const char 
+*color_code(TerminalColor color)
+{
+	static char code[8];
+	snprintf(code, sizeof(code), "\033[%dm", color);
+	return code;
+}
 
 /**
  * @brief The shell loop
@@ -24,9 +41,37 @@ loop(void)
 	char *line;
 	char **args;
 	int status;
-
+	char cwd[CWD_BUF_SZ];
 	do {
-		printf("sh-ovel > ");
+		if (getcwd(cwd, sizeof(cwd))) {
+			char *dir = strrchr(cwd, '/');
+			if (dir) {
+				printf("%ssh-ovel%s ",
+					color_code(COLOR_BRIGHT_GREEN),
+					color_code(COLOR_RESET)
+				);
+				printf("%s%s%s ",
+					color_code(COLOR_BRIGHT_BLUE),
+					dir + 1,
+					color_code(COLOR_RESET)
+				);
+				printf("%s> ",
+					color_code(COLOR_BRIGHT_GREEN)
+				);
+			} else {
+				printf("%ssh-ovel%s ",
+					color_code(COLOR_BRIGHT_GREEN),
+					color_code(COLOR_RESET)
+				);
+				printf("%s> ",
+					color_code(COLOR_BRIGHT_GREEN)
+				);
+			}
+			printf("%s", color_code(COLOR_RESET));
+		} else {
+			perror("getcwd() error");
+			return;
+		}
 		line = read_line();
 		args = split_line(line);
 		status = execute(args);
